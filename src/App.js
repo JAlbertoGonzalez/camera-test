4cae2884949b787d426a48b50a4ba44bdaa38e2c
Alberto Torre <45895462+JAlbertoGonzalez@users.noreply.github.com>
2023-06-15 12:29:45 +0200

    demo implementation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
+import { useRef } from 'react';
 import './App.css';
+import { Button, Input, TextField } from '@mui/material';
+import { useState } from 'react';
 
 function App() {
+  const ref = useRef()
+
+  const [files, setFiles] = useState([])
+  const [selectedFile, setSelectedFile] = useState(-1)
+
+  function uploadCapturedImage(e) {
+    const filesUploaded = e.target.files
+    for (const file of filesUploaded) {
+      setFiles(oldFiles => [...oldFiles, file])
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <input
+          type="file" ref={ref}
+          name="camera-upload"
+          accept="image/*"
+          capture="camera"
+          style={{ display: 'none' }}
+          onChange={uploadCapturedImage} />
+
+        <Button variant='contained' onClick={() => ref.current.click()}>
+          Subir ticket
+        </Button>
+
+        <p>Uploaded images:</p>
+        {files.map((file, index) => <Button variant='outlined' onClick={() => setSelectedFile(index)}>{file.name}</Button>)}
+
+        {selectedFile > -1 && <p style={{ width: '50%', padding: '12px' }}>
+          <img alt="preview" src={URL.createObjectURL(files[selectedFile])} style={{ width: '100%' }} />
+        </p>}
       </header>
     </div>
   );
